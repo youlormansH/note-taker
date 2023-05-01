0541e9786b8a01cf487efdf7705b65b143968b5c
youlormansH <128164751+youlormansH@users.noreply.github.com>
2023-05-01 01:30:15 -0400

    starting the work on the route.get

--- Develop/routes/ApiRoute.js
+++ Develop/routes/ApiRoute.js
@@ -3,3 +3,9 @@ const router = require ("express").Router()
 const { json } = require("express");
 const fs = require("fs");
 const { v4: uuidv4 } = require('uuid');
+
+router.get("/notes",function (req,res){
+    fs.readFile("db/db.json", "utf-8", function (err, data){
+err ? console.log (err) : res.json(JSON.parse(data))
+    })
+});
