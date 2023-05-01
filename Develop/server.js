8dcc8cd4b8b138e1669c33ff79b302eba665c40c
youlormansH <128164751+youlormansH@users.noreply.github.com>
2023-05-01 01:23:15 -0400

    finish the server.js

--- Develop/server.js
+++ Develop/server.js
@@ -5,3 +5,13 @@ const html= require("./routes/route")
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+app.use('/',html)
+
+app.listen(PORT, () => {
+    console.log(`App listening at ${PORT}`);
+});
